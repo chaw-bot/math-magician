3553bf33b7d120fe946052a32e85097b13b9c263
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-21 12:16:41 +0200

    Import calculate function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,71 @@
 import React from 'react';
-import calculate from '../logic/calculator';
-import operate from '../logic/operate';
+import calculate from '../logic/calculate';
+import Buttons from './buttons';
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      calcObj: {},
+    };
+
+    this.handleCalculate = this.handleCalculate.bind(this);
+  }
+
+  handleCalculate(calcObj, text) {
+    this.setState({ calcObj: calculate(calcObj, text) });
   }
 
   render() {
+    const { calcObj } = this.state;
+
+    let calcDisplay = '0';
+    if (
+      Object.keys(calcObj).length === 0
+      || (calcObj.total === null
+        && calcObj.next === null
+        && calcObj.operation === null)
+    ) {
+      calcDisplay = '0';
+    } else {
+      calcDisplay = calcObj.next != null ? calcObj.next : calcObj.total;
+    }
+
     return (
       <div className="Calc-body">
-        <div className="Calc-display">0</div>
+        <div className="Calc-display">{calcDisplay}</div>
         <div className="Calc-btns">
           <div className="Grey-btns">
             <div className="Grey-btns1">
-              <div className="Calc-btn">AC</div>
-              <div className="Calc-btn">+/-</div>
-              <div className="Calc-btn">%</div>
-              <div className="Calc-btn">7</div>
-              <div className="Calc-btn">8</div>
-              <div className="Calc-btn">9</div>
-              <div className="Calc-btn">4</div>
-              <div className="Calc-btn">5</div>
-              <div className="Calc-btn">6</div>
-              <div className="Calc-btn">1</div>
-              <div className="Calc-btn">2</div>
-              <div className="Calc-btn">3</div>
+              <Buttons text="AC" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons
+                text="+/-"
+                onClick={this.handleCalculate}
+                Obj={calcObj}
+              />
+              <Buttons text="%" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="7" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="8" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="9" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="4" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="5" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="6" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="1" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="2" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="3" onClick={this.handleCalculate} Obj={calcObj} />
             </div>
             <div className="Grey-btns2">
-              <div className="Calc-btn">0</div>
-              <div className="Calc-btn">.</div>
+              <Buttons text="0" onClick={this.handleCalculate} Obj={calcObj} />
+              <Buttons text="." onClick={this.handleCalculate} Obj={calcObj} />
             </div>
           </div>
 
           <div className="Orange-btns">
-            <div className="Calc-btn">÷</div>
-            <div className="Calc-btn">x</div>
-            <div className="Calc-btn">-</div>
-            <div className="Calc-btn">+</div>
-            <div className="Calc-btn">=</div>
+            <Buttons text="÷" onClick={this.handleCalculate} Obj={calcObj} />
+            <Buttons text="x" onClick={this.handleCalculate} Obj={calcObj} />
+            <Buttons text="-" onClick={this.handleCalculate} Obj={calcObj} />
+            <Buttons text="+" onClick={this.handleCalculate} Obj={calcObj} />
+            <Buttons text="=" onClick={this.handleCalculate} Obj={calcObj} />
           </div>
         </div>
       </div>
