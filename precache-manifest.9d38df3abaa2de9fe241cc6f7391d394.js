7aeb809a66f46e4825dc1e2744ae5b783826a551
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-28 00:45:51 +0200

    Updates

---
+++ precache-manifest.9d38df3abaa2de9fe241cc6f7391d394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580c7d86b69a2efb85bf1957697b529c",
+    "url": "/math-magician/index.html"
+  },
+  {
+    "revision": "8c52567904e12aaf027e",
+    "url": "/math-magician/static/css/main.42d84b15.chunk.css"
+  },
+  {
+    "revision": "c1263f92aec044e462bd",
+    "url": "/math-magician/static/js/2.335ddcda.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/math-magician/static/js/2.335ddcda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c52567904e12aaf027e",
+    "url": "/math-magician/static/js/main.77516c31.chunk.js"
+  },
+  {
+    "revision": "26afda3be59619fc3ec7",
+    "url": "/math-magician/static/js/runtime-main.347af288.js"
+  }
+]);
