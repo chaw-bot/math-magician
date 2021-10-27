c28c00b93b59543ad36871e2ddef859a95e56a2e
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 22:32:00 +0200

    Link all pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,9 +14,20 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+          <Route path="/quotes">
+            <Quote />
+          </Route>
+        </Switch>
+      </>
     );
   }
 }
