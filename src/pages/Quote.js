1a8a5f9eebdb961edec54998023e9b8f73e92d42
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 22:32:45 +0200

    Create quote page

---
+++ src/pages/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote-page">
+    <h1>today&apos;s quote....</h1>
+    <p>
+      When the seas of life get rough, remember your Lifeguard walks on water.
+      - Unknown
+    </p>
+  </div>
+);
+
+export default Quote;
