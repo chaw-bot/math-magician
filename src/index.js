1ff40708cac6e33116c68b742a442d789b0155ad
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 22:30:29 +0200

    Add screenshots to README

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.render(checkout
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
   document.getElementById('root'),
 );
