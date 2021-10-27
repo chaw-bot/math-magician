f3fb54d7ec50b579d824d0e5597369252b3e8bfa
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 22:32:13 +0200

    Create header

---
+++ src/pages/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+      classname: 'homePage',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calculator',
+      classname: 'calcPage',
+    },
+    {
+      id: 2,
+      path: '/quotes',
+      text: 'Quotes',
+      classname: 'quotePage',
+    },
+  ];
+
+  return (
+    <div className="nav">
+      <nav className="navBar">
+        <h1 className="title">
+          MathMagicians
+        </h1>
+        <ul className="otherPages">
+          {links.map((link) => (
+            <li className={link.classname} key={link.id}>
+              <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <footer className="footer">
+        <p>Created by Chawanzi Ng&apos;uni</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Header;
