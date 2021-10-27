cf5b9e9c87316489b8c12b9542393cd3504aa5ae
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 22:33:03 +0200

    Style app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,48 +13,53 @@ const Calculator = () => {
   let calcDisplay = '0';
   if (
     Object.keys(calcObj).length === 0
-      || (calcObj.total === null
-        && calcObj.next === null
-        && calcObj.operation === null)
+    || (calcObj.total === null
+      && calcObj.next === null
+      && calcObj.operation === null)
   ) {
     calcDisplay = '0';
   } else {
     calcDisplay = calcObj.next != null ? calcObj.next
-      : calcObj.next != null ? calcObj.total
+      : calcObj.total != null ? calcObj.total
         : calcObj.operation;
   }
 
   return (
-    <div className="Calc-body">
-      <div className="Calc-display">{calcDisplay}</div>
-      <div className="Calc-btns">
-        <div className="Grey-btns">
-          <div className="Grey-btns1">
-            <Buttons text="AC" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="+/-" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="%" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="7" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="8" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="9" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="4" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="5" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="6" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="1" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="2" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="3" onClick={handleCalculate} Obj={calcObj} />
+    <div className="calc-page">
+      <div>
+        <h2 className="title2">Let&apos;s do some math!</h2>
+      </div>
+      <div className="Calc-body">
+        <div className="Calc-display">{calcDisplay}</div>
+        <div className="Calc-btns">
+          <div className="Grey-btns">
+            <div className="Grey-btns1">
+              <Buttons text="AC" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="+/-" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="%" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="7" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="8" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="9" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="4" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="5" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="6" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="1" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="2" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="3" onClick={handleCalculate} Obj={calcObj} />
+            </div>
+            <div className="Grey-btns2">
+              <Buttons text="0" onClick={handleCalculate} Obj={calcObj} />
+              <Buttons text="." onClick={handleCalculate} Obj={calcObj} />
+            </div>
           </div>
-          <div className="Grey-btns2">
-            <Buttons text="0" onClick={handleCalculate} Obj={calcObj} />
-            <Buttons text="." onClick={handleCalculate} Obj={calcObj} />
-          </div>
-        </div>
 
-        <div className="Orange-btns">
-          <Buttons text="÷" onClick={handleCalculate} Obj={calcObj} />
-          <Buttons text="x" onClick={handleCalculate} Obj={calcObj} />
-          <Buttons text="-" onClick={handleCalculate} Obj={calcObj} />
-          <Buttons text="+" onClick={handleCalculate} Obj={calcObj} />
-          <Buttons text="=" onClick={handleCalculate} Obj={calcObj} />
+          <div className="Orange-btns">
+            <Buttons text="÷" onClick={handleCalculate} Obj={calcObj} />
+            <Buttons text="x" onClick={handleCalculate} Obj={calcObj} />
+            <Buttons text="-" onClick={handleCalculate} Obj={calcObj} />
+            <Buttons text="+" onClick={handleCalculate} Obj={calcObj} />
+            <Buttons text="=" onClick={handleCalculate} Obj={calcObj} />
+          </div>
         </div>
       </div>
     </div>
